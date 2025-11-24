f116c3f65fa16e0161136257d410145cc6430c73
Eishmeeta <eishmeetak77@gmail.com>
2025-11-24 15:42:33 +0530

    -added codes for searching

---
+++ searching/binarysearch.py
@@ -0,0 +1,18 @@
+#efficient in sorted array by repeatedly dividing the array into half
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) -1
+    while low <= high:
+        mid = (low + high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else: 
+            high = mid + 1
+
+    return -1
+
+#use when array is sorted 
+# time complexity O(log n)
---
+++ searching/linearsearch.py
@@ -0,0 +1,13 @@
+#linear search algorithm
+#checks elements in a list sequwntially until the desired element is found
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+
+#use when the array is small or uhsorted 
+#time complexity: O(n)
+#space complexity: O(1)
+
